d6434bdd614d2b7565fa59e1f8a6a206937dcb9a
MaykeKerssens <93595867+MaykeKerssens@users.noreply.github.com>
2023-09-14 13:41:51 +0200

    Temporary turned off Inertia

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,10 +13,10 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @inertiaHead
+        {{-- @inertiaHead --}}
     </head>
     <body class="font-sans antialiased">
-        @inertia
+        {{-- @inertia --}}
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
