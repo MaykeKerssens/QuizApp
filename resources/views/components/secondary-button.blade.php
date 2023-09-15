c4bc51038fd48254bfa0b765416c170be6d79b35
MaykeKerssens <93595867+MaykeKerssens@users.noreply.github.com>
2023-09-15 22:38:37 +0200

    Added topics from database to dashboard pt.2

--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'button', 'class' => 'bg-yellow-400 p-2 px-3 rounded-full items-center font-semibold min-w-full']) }}>
+<button {{ $attributes->merge(['type' => 'button', 'class' => 'bg-yellow-400 text-lg p-2 px-3 rounded-full items-center font-semibold min-w-full']) }}>
     {{ $slot }}
 </button>
