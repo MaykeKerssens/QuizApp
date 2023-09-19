969788bf3cda1dd80c4d2121a89783fd13d1285a
MaykeKerssens <93595867+MaykeKerssens@users.noreply.github.com>
2023-09-19 14:11:13 +0200

    Brought submit button to the bottem of the page
    (this was a pain in the ass)

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -1,56 +1,54 @@
 <x-app-layout>
 
-    <div class="pt-6 mx-6 min-h-screen flex flex-col">
-        <div class="text-gray-100 text-3xl mb-6 border-b border-gray-100 flex justify-between">
-            <h2 class="">{{ $question->topic->name }}:</h2>
-            <h2 class="text-lg">{{ $counter + 1 }}/{{ $questionsAmount }}</h2>
-        </div>
-
-
-        <h2>{{ $counter + 1 }}. {{ $question->text }}</h2>
-
-
-
-        <form action="{{ route('saveAnswer')}}">
+    <div class="pt-6 mx-6 flex h-full flex-col">
+        <form action="{{ route('saveAnswer') }}" class="flex flex-col justify-between h-full">
             @csrf
-            <div class="space-y-4">
-                @foreach ($answers as $answer)
-                    <x-secondary-button class="answer-button" data-answer-id="{{ $answer->id }}">
-                        {{ $answer->text }}
+            <div> <!-- everything -->
+                <div class="text-gray-100 text-3xl mb-6 border-b border-gray-100 flex justify-between">
+                    <h2 class="">{{ $question->topic->name }}:</h2>
+                    <h2 class="text-lg">{{ $counter + 1 }}/{{ $questionsAmount }}</h2>
+                </div>
 
-                    </x-secondary-button>
-                @endforeach
+                <h2>{{ $counter + 1 }}. {{ $question->text }}</h2>
 
+                <div class="min-h-full flex flex-col justify-between ">
+                    <div class="space-y-4">
+                        @foreach ($answers as $answer)
+                            <x-secondary-button class="answer-button" data-answer-id="{{ $answer->id }}">
+                                {{ $answer->text }}
 
-                <script>
-                    const buttons = document.querySelectorAll('button');
+                            </x-secondary-button>
+                        @endforeach
 
-                    buttons.forEach(button => {
-                        button.addEventListener('click', () => {
-                            // Remove the selected classes from all buttons
-                            buttons.forEach(btn => {
-                                btn.classList.remove('bg-yellow-200');
-                                btn.classList.add('bg-yellow-400');
+                        <script>
+                            const buttons = document.querySelectorAll('button');
+
+                            buttons.forEach(button => {
+                                button.addEventListener('click', () => {
+                                    // Remove the selected classes from all buttons
+                                    buttons.forEach(btn => {
+                                        btn.classList.remove('bg-yellow-200');
+                                        btn.classList.add('bg-yellow-400');
+                                    });
+
+                                    // Add selected classes to the clicked button
+                                    button.classList.remove('bg-yellow-400');
+                                    button.classList.add('bg-yellow-200');
+
+                                    // Set the value of the hidden input to the selected answer's ID
+                                    const selectedAnswerId = button.getAttribute('data-answer-id');
+                                    document.querySelector('#selected_answer_id').value = selectedAnswerId;
+                                });
                             });
-
-                            // Add selected classes to the clicked button
-                            button.classList.remove('bg-yellow-400');
-                            button.classList.add('bg-yellow-200');
-
-                            // Set the value of the hidden input to the selected answer's ID
-                            const selectedAnswerId = button.getAttribute('data-answer-id');
-                            document.querySelector('#selected_answer_id').value = selectedAnswerId;
-                        });
-                    });
-                </script>
+                        </script>
+                    </div>
+                </div>
             </div>
 
-
-
-            @if ($question->type == "multipleChoice")
+            <!-- hidden inputs -->
+            @if ($question->type == 'multipleChoice')
                 <input type="hidden" name="selected_answer_id" id="selected_answer_id" value="">
             @else
-                {{-- //TODO: save openAnswer response here --}}
                 <input type="hidden" name="response" value="">
             @endif
 
@@ -58,11 +56,11 @@
             <input type="hidden" name="questionsAmount" value="{{ $questionsAmount }}">
             <input type="hidden" name="counter" value="{{ $counter }}">
 
-            <x-primary-button>Submit</x-primary-button>
+            <div> <!-- submit button -->
+                <x-primary-button class="">Submit</x-primary-button>
+            </div>
+
         </form>
-
-
-
     </div>
 
 </x-app-layout>
